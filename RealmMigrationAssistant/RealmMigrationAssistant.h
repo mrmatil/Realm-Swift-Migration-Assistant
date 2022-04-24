ea0a01cf84e34e826396869bc45b515c05758e9d
Mateusz Łukasiński <mateusz.lukasinski@icloud.com>
2022-04-24 18:48:01 +0200

    Initial Commit

---
+++ RealmMigrationAssistant/RealmMigrationAssistant.h
@@ -0,0 +1,18 @@
+//
+//  RealmMigrationAssistant.h
+//  RealmMigrationAssistant
+//
+//  Created by Mateusz Łukasiński on 24/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealmMigrationAssistant.
+FOUNDATION_EXPORT double RealmMigrationAssistantVersionNumber;
+
+//! Project version string for RealmMigrationAssistant.
+FOUNDATION_EXPORT const unsigned char RealmMigrationAssistantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmMigrationAssistant/PublicHeader.h>
+
+
